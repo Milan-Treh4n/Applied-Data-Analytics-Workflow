3f2f1491f6167a06095302fe1cd501acb94ada3c
Milan-Treh4n <trehanmilan5@gmail.com>
2025-12-29 19:19:25 +0000

    renaming scripts for clarity

---
+++ plots/deaths_bar_2019.py
@@ -0,0 +1,30 @@
+import matplotlib.pyplot as plt
+import pandas as pd
+from pathlib import Path
+
+# Define the path to 2019 deaths data CSV file
+PROJECT_ROOT = Path(__file__).resolve().parents[1]
+
+df = pd.read_csv(
+    PROJECT_ROOT / "data" / "processed" / "uk_business_deaths_2019_clean.csv"
+)
+
+df = df.sort_values("Number of Business Deaths (2019)", ascending=False)
+
+# remove first 4 non-regional entries
+df = df.iloc[4:]
+
+# Create a horizontal bar chart
+plt.figure(figsize=(10, 8))
+plt.barh(df["Region"], df["Number of Business Deaths (2019)"], color='skyblue')
+plt.xlabel("Number of Business Deaths (2019)")
+plt.title("Business Deaths by Region in the UK (2019)")
+plt.gca().invert_yaxis()  # Invert y-axis to have the highest values at the top
+plt.tight_layout()
+
+# Save the plot
+plots = PROJECT_ROOT / "plots"
+plots.mkdir(exist_ok=True)
+
+plt.savefig(plots / "business_deaths_top_regions_2019.png", dpi=300)
+plt.show()
