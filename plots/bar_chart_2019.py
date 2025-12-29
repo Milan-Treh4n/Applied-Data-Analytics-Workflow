e331438ecf0a8ae930301a49bf832960d3e90f4b
Milan-Treh4n <trehanmilan5@gmail.com>
2025-12-29 19:01:37 +0000

    adding necessary documentation

--- plots/bar_chart_2019.py
+++ plots/bar_chart_2019.py
@@ -2,11 +2,14 @@ import matplotlib.pyplot as plt
 import pandas as pd
 from pathlib import Path
 
+# Define project root
 PROJECT_ROOT = Path(__file__).resolve().parents[1]
 
+# load 2019 births
 df = pd.read_csv(
     PROJECT_ROOT / "data" / "processed" / "uk_business_births_2019_clean.csv"
 )
+# sort by number of births descending 
 
 df = df.sort_values("Number of Business Births (2019)", ascending=False)
 
@@ -16,6 +19,7 @@ df = df.iloc[4:]
 top_n = min(15, len(df))
 df = df.head(top_n)
 
+# create horizontal bar chart
 plt.figure(figsize=(10, 6))
 plt.barh(df["Geography Name"], df["Number of Business Births (2019)"])
 plt.xlabel("Number of Business Births")
